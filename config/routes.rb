50431cd97b28bd1a22214b3d2e2c0cdcd492246e
jimmy <jamesmc21998@gmail.com>
2023-08-15 14:12:17 -0500

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :pets
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
